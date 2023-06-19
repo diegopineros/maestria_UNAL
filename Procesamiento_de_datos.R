b2a36cc9e99bbddccebf078296bdf7b554285cff
Miguel Pineros <miguelpineros@hotmail.com>
2023-06-19 17:17:23 -0500

    Ajuste de varibles en el codigo base definitivas junio-23

--- Procesamiento_de_datos.R
+++ Procesamiento_de_datos.R
@@ -31,57 +31,64 @@ for (i in c(2018, 2020, 2022)){#2018, 2019, 2020, 2021, 2022
   ###########################  Cargue de hogares  ################################
   ##############################################################################'#
   
-  fla_hog <- base_hogar %>% select(DIRECTORIO,SECUENCIA_P,P5666,P8536,P1698,
-                                   I_HOGAR,I_UGASTO,I_OU,CANT_PERSONAS_HOGAR)#FEX_C, P1698S1, PERCAPITA
-  
-  fila_viv <- base_vivienda %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,ORDEN,
+  fila_viv <- base_vivienda %>% select(DIRECTORIO,ORDEN,
                                        P1_DEPARTAMENTO,CLASE,P1070,P8520S1)#,FEX_C
   
-  fla_hog <- base_hogar %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,SECUENCIA_P,P5666,
+  fla_hog <- base_hogar %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,P5666,
                                    P8536,P1698,P1698S1,I_HOGAR,I_UGASTO,PERCAPITA,
                                    I_OU,CANT_PERSONAS_HOGAR)
   
-  fla_hog$SECUENCIA_ENCUESTA_A <- fla_hog$SECUENCIA_ENCUESTA
-  fla_hog$SECUENCIA_ENCUESTA <- fla_hog$SECUENCIA_P
+
   
   ##############################################################################'#
   ##########################  Agrupación hogares  ###############################
   ##############################################################################'#
   
-  base_combustible <- inner_join(fila_viv,fla_hog, by= c("DIRECTORIO", "SECUENCIA_ENCUESTA"))
-  base_combustible$SECUENCIA_P <- base_combustible$SECUENCIA_ENCUESTA_A
+  base_combustible <- inner_join(fila_viv,fla_hog, by= c("DIRECTORIO"))
   base_combustible <- base_combustible %>% filter(CLASE==2)
   
   ##############################################################################'#
   ############################  Cargue personas  #################################
   ##############################################################################'#
   
-  educ <- base_educ %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,SECUENCIA_P,ORDEN,
+  educ <- base_educ %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,SECUENCIA_P,
                                P8587,P8587S1)
-  pobl <- base_poblacion %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,SECUENCIA_P,ORDEN,
+  pobl <- base_poblacion %>% select(DIRECTORIO,SECUENCIA_ENCUESTA,SECUENCIA_P,
                                     P6020,P6040,P6051)
   
-  base_pop <- inner_join(x = educ,y = pobl,by=c("DIRECTORIO","SECUENCIA_P","ORDEN"))
-  base_pop <- base_pop %>% group_by(DIRECTORIO,SECUENCIA_P,ORDEN) %>% mutate(avg_edad=mean(P6040))
-  base_pop <- base_pop %>% group_by(DIRECTORIO,SECUENCIA_P,ORDEN) %>% 
-    mutate(Jefe_sex=ifelse(P6051==1,P6020,0),Jefe_edad=ifelse(P6051==1,P6040,0),
-           menores=ifelse(P6040<=18,1,0),Jefe_educ_sec=ifelse(P8587==4|P8587==5&P6051==1,1,0),
-           Jefe_educ_ter=ifelse(P8587>=6&P6051==1,1,0))
+  base_pop <- inner_join(x = educ,y = pobl,by=c("DIRECTORIO","SECUENCIA_ENCUESTA",
+                                                "SECUENCIA_P"))
+  
+  base_pop <- base_pop %>% group_by(DIRECTORIO,SECUENCIA_P) %>% mutate(avg_edad=mean(P6040))
+  base_pop$Jefe_educ_sec <- 0
+  base_pop$Jefe_educ_ter <- 0
+  base_pop$menores <- 0
+  base_pop$Jefe_edad <- 0
+  base_pop$Jefe_sex <- 0
+  base_pop$personas <-1
+
+  base_pop <- as.data.table(base_pop)
+  base_pop <- base_pop[P6051==1,Jefe_sex:=P6020]
+  base_pop <- base_pop[P6051==1,Jefe_edad:=P6040]
+  base_pop <- base_pop[P6040<=18,menores:=1]
+  base_pop <- base_pop[P8587==4|P8587==5&P6051==1,Jefe_educ_sec:=1]
+  base_pop <- base_pop[P8587>=6&P6051==1,Jefe_educ_ter:=1]
   
   ##############################################################################'#
   ##########################  Agrupacion personas  ###############################
   ##############################################################################'#
   
-  base_pop_clear <- base_pop %>% summarise(Jefe_sex=max(Jefe_sex),
-                                           Jefe_edad=max(Jefe_edad),
-                                           menores=sum(menores),
-                                           Jefe_educ_sec=max(Jefe_educ_sec),
-                                           Jefe_educ_ter=max(Jefe_educ_ter),
-                                           avg_edad=mean(avg_edad))
+  base_pop_clear <- base_pop %>% group_by(DIRECTORIO,SECUENCIA_P) %>% 
+    summarise(Jefe_sex=max(Jefe_sex),Jefe_edad=max(Jefe_edad),menores=sum(menores),
+              Jefe_educ_sec=max(Jefe_educ_sec),Jefe_educ_ter=max(Jefe_educ_ter),
+              avg_edad=mean(avg_edad), personas=sum(personas))
   
-  base <- inner_join(base_combustible,base_pop_clear,by=c("DIRECTORIO","SECUENCIA_P",
-                                                          "ORDEN"))
+  base_pop_clear$SECUENCIA_ENCUESTA <- base_pop_clear$SECUENCIA_P
   
+  base_pop_clear <- base_pop_clear %>% select(-SECUENCIA_P)
+  
+  base <- inner_join(base_combustible,base_pop_clear,by=c("DIRECTORIO","SECUENCIA_ENCUESTA"))
+
   ##############################################################################'#
   ######################  Ajuste nombres de variables  ###########################
   ##############################################################################'#
@@ -93,8 +100,8 @@ for (i in c(2018, 2020, 2022)){#2018, 2019, 2020, 2021, 2022
   ##########################  Export datos modelo  ###############################
   ##############################################################################'#
   
-  write.table(base,paste("./base_paper", i, '.csv', sep=''), sep = ";",dec=".",row.names = FALSE)
-  print(paste("./base_paper", i, '.csv', sep=''))
+  write.table(base,paste("./base_paper",i,'.csv',sep=''),sep = ";",dec=".",row.names=FALSE)
+  print(paste("./base_paper",i,'.csv',sep=''))
 }
 
 ##############################################################################'
